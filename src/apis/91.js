7b2a002147388326e690b9cfde9a2b12a0f13e2e
lucifer <xifa@tsign.cn>
2021-04-27 15:43:43 +0800

    feat: go

--- src/apis/91.js
+++ src/apis/91.js
@@ -1,10 +1,10 @@
-import axios from 'axios'
+import request from './request'
 // const host = 'https://a91algo.herokuapp.com'
 const host = 'http://localhost:3000'
 export function getBasicLecture() {
-  return axios.get(`${host}/api/v1/lectures/basic`).then(res => res.data)
+  return request(`${host}/api/v1/lectures/basic`).then(res => res.data)
 }
 
 export function getBasicLectureDetails(id) {
-  return axios.get(`${host}/api/v1/lectures/basic/${id}`).then(res => res.data)
+  return request(`${host}/api/v1/lectures/basic/${id}`).then(res => res.data)
 }
