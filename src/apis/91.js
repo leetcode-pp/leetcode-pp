dd25da16b3cc17e7111d72cc228735266b9ba40b
lucifer <xifa@tsign.cn>
2021-04-27 15:59:09 +0800

    feat: go

--- src/apis/91.js
+++ src/apis/91.js
@@ -1,10 +1,13 @@
 import request from './request'
-// const host = 'https://a91algo.herokuapp.com'
-const host = 'http://localhost:3000'
+
 export function getBasicLecture() {
-  return request(`${host}/api/v1/lectures/basic`).then(res => res.data)
+  return request({
+    url: '/api/v1/lectures/basic'
+  }).then(res => res.data)
 }
 
 export function getBasicLectureDetails(id) {
-  return request(`${host}/api/v1/lectures/basic/${id}`).then(res => res.data)
+  return request({
+    url: '/api/v1/lectures/basic/${id}'
+  }).then(res => res.data)
 }
--- src/apis/request.js
+++ src/apis/request.js
@@ -1,9 +1,11 @@
 import { message } from 'ant-design-vue'
 import axios from 'axios'
-// const host = 'https://a91algo.herokuapp.com'
-const host = 'http://localhost:3000'
-export default function(url, options) {
-  return axios(`${host}${url}`, options)
+const host =
+  process.env.NODE_ENV === 'production'
+    ? 'https://a91algo.herokuapp.com'
+    : 'http://localhost:3000'
+export default function(options) {
+  return axios({ ...options, url: `${host}${options.url}` })
     .then(res => {
       if (res.success) return res.data
       throw res
