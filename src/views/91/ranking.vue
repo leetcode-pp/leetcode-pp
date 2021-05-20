3a34ace279d591a40eb855924dc351b1da66d596
robot <xifa@tsign.cn>
2021-05-20 13:15:04 +0800

    feat: ranking

--- src/views/91/ranking.vue
+++ src/views/91/ranking.vue
@@ -1,28 +1,42 @@
 <template>
-  <a-list item-layout="horizontal" :data-source="rankings" class="teachers">
-    <a-list-item slot="renderItem" slot-scope="item">
-      <a-list-item-meta>
-        <div slot="description">
-          累计打卡 {{ item.count }} 次
-          <a-icon v-if="item.allCheck" class="all-check" type="carry-out" />
+  <div>
+    <div class="info">
+      满勤：<a-icon class="all-check" type="carry-out" />
+      注：如果未满勤会自动使用补签卡，使用后满勤也算满勤
+    </div>
+    <div class="info">
+      七天内未打卡：<a-icon class="no-check" type="exclamation-circle" />
+    </div>
+    <a-list item-layout="horizontal" :data-source="rankings" class="teachers">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <a-list-item-meta>
+          <div slot="description">
+            累计打卡 {{ item.count }} 次
+            <a-icon v-if="item.allCheck" class="all-check" type="carry-out" />
+            <a-icon
+              v-if="item.noCheck"
+              class="no-check"
+              type="exclamation-circle"
+            />
+          </div>
+          <div slot="title">
+            <a :href="'https://github.com/' + item.login"
+              >{{ item.name || item.login }}
+            </a>
+          </div>
+          <a-avatar slot="avatar" :src="item.avatar_url" />
+        </a-list-item-meta>
+        <div class="more">
+          <a-button
+            type="link"
+            :href="'https://github.com/' + item.login"
+            target="_blank"
+            >关注 ta</a-button
+          >
         </div>
-        <div slot="title">
-          <a :href="'https://github.com/' + item.login"
-            >{{ item.name || item.login }}
-          </a>
-        </div>
-        <a-avatar slot="avatar" :src="item.avatar_url" />
-      </a-list-item-meta>
-      <div class="more">
-        <a-button
-          type="link"
-          :href="'https://github.com/' + item.login"
-          target="_blank"
-          >关注 ta</a-button
-        >
-      </div>
-    </a-list-item>
-  </a-list></template
+      </a-list-item>
+    </a-list>
+  </div></template
 >
 
 <script>
@@ -35,6 +49,10 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+.info {
+  margin: 10px 0;
+  text-align: left;
+}
 .more {
   display: flex;
   justify-content: flex-end;
@@ -44,8 +62,15 @@ export default {
     line-height: 24px;
   }
 }
-.all-check {
+.all-check,
+.no-check {
   margin-left: 10px;
-  font-size: 20px;
+  font-size: 16px;
+}
+.all-check {
+  color: rgb(0, 128, 79);
+}
+.no-check {
+  color: rgba(255, 0, 0, 0.664);
 }
 </style>
