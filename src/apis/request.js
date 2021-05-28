de8672296faefa196f523beb0bcb9da4a0aff875
robot <xifa@tsign.cn>
2021-05-28 11:29:07 +0800

    fix: abs path

--- src/apis/request.js
+++ src/apis/request.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 import { hostname } from '@/config/index'
 
 const host =
-  process.env.NODE_ENV === 'production' ? hostname : 'http://localhost:3000'
+  process.env.NODE_ENV === 'production'
+    ? `https://${hostname}`
+    : 'http://localhost:3000'
 export default function(options) {
   return axios({
     ...options,
