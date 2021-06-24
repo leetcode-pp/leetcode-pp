e0d8da78be451de32dbb5c5cb4103a513cffa13b
robot <xifa@tsign.cn>
2021-06-24 10:19:09 +0800

    feat: go

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,8 @@ module.exports = {
   startTime: startTime.getTime(),
   clientId: 'c16b80e7b58a5a007157',
   originalHostname: 'leetcode-solution.cn',
-  hostname: 'algo91.herokuapp.com',
+  // hostname: 'algo91.herokuapp.com',
+  hostname: 'algo91-4.herokuapp.com',
   // hostname: 'a91algo.herokuapp.com',
   leetcodeConfig: {
     _91UsernameLsName: 'login', // 在91网站中存lc用户名的 localStorage 的键名
