eb1a8ffc6263b902ee11bf3549d935fcd2585ba2
robot <xifa@tsign.cn>
2021-12-12 00:06:41 +0800

    fix: typo

--- src/views/91/91algo.vue
+++ src/views/91/91algo.vue
@@ -450,7 +450,7 @@ async function loadComment({
   const gitalk = new Gitalk({
     clientID,
     clientSecret,
-    repo: '91alg-5-daily-check', // The repository of store comments,
+    repo: '91alg-6-daily-check', // The repository of store comments,
     owner: 'leetcode-pp',
     admin: ['azl397985856'],
     body: `
