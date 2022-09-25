1064e5bcefc0d0b03ec2290112cf70f355c03481
robot <xifa@tsign.cn>
2022-09-26 00:00:57 +0800

    chore: host

--- src/config/index.js
+++ src/config/index.js
@@ -3,12 +3,12 @@ const hostnamePool = [
   // 'algo91-4.herokuapp.com',
   // 'algo-6.herokuapp.com' // cpv3kbrhw
   // 'algo-5.herokuapp.com' // fe_lucife  @ sina
-  // 'algo91.herokuapp.com' // fe-lucifer
+  'algo91.herokuapp.com' // fe-lucifer
   // 'algo91-no-sub.herokuapp.com' // fe-lucifer
   // 'a91algo.herokuapp.com' // azl
   // 'leetcode-pp.herokuapp.com' // azl
   // 'localhost'
-  'algo91bak1.herokuapp.com' // fe_lucifer @ firefox
+  // 'algo91bak1.herokuapp.com' // fe_lucifer @ firefox
 ]
 const hostname =
   hostnamePool[
