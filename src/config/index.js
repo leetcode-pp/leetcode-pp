5d06048e50d63e68b8140d91f6eb80c61828bdcc
robot <xifa@tsign.cn>
2023-04-06 01:49:46 +0800

    go

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const startTime = new Date('2023-02-14T00:00:00.000+08:00')
 const hostnamePool = [
-  // 'leetcode-pp-node-seven.vercel.app'
-  'leetcode-pp-node-91-algo.vercel.app'
+  'leetcode-pp-node-seven.vercel.app'
+  // 'leetcode-pp-node-91-algo.vercel.app'
   // 'algo91-4.herokuapp.com',
   // 'algo-6.herokuapp.com' // cpv3kbrhw
   // 'algo-5.herokuapp.com' // fe_lucife  @ sina
